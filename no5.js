20a21d612b0a554eb3602d32da3f0c6fb5d287eb
Ryandi Pratama Purba <purbaryandi@gmail.com>
2021-09-01 08:24:11 +0700

    new

--- no5.js
+++ no5.js
@@ -1,14 +1,9 @@
 const fs = require('fs')
 var inventory = require('./data/inventory.json')
 
-// 1. Find items in Meeting Room, and save it to items.json.
-
-const soal1 = (data) => {
- let tes = data.map(item =>
-  item).flat().filter(item => item.placement.name == "Meeting Room")
- console.log(tes);
- const jsonString = JSON.stringify(tes)
- fs.writeFile('./data/item.json', jsonString, err => {
+const createFileJson = (name, data) => {
+ const jsonString = JSON.stringify(data)
+ fs.writeFile(`./data/${name}.json`, jsonString, err => {
   if (err) {
    console.log('Error writing file', err)
   } else {
@@ -16,22 +11,24 @@ const soal1 = (data) => {
   }
  })
 }
+
+// 1. Find items in Meeting Room, and save it to items.json.
+
+const soal1 = (data) => {
+ let result = data.map(item =>
+  item).flat().filter(item => item.placement.name == "Meeting Room")
+ console.log(result);
+ createFileJson("items", result)
+}
 // soal1(inventory)
 
 // 2.Find all electronic devices, and save it to electronic.json.
 
 const soal2 = (data) => {
- let tes = data.map(item =>
+ let result = data.map(item =>
   item).flat().filter(item => item.type == "electronic")
- console.log(tes);
- const jsonString = JSON.stringify(tes)
- fs.writeFile('./data/electronic.json', jsonString, err => {
-  if (err) {
-   console.log('Error writing file', err)
-  } else {
-   console.log('Successfully wrote file')
-  }
- })
+ console.log(result);
+ createFileJson("electronic", result)
 }
 
 // soal2(inventory)
@@ -39,53 +36,30 @@ const soal2 = (data) => {
 
 //3. Find all furnitures, and save it to furnitures.json.
 const soal3 = (data) => {
- let tes = data.map(item =>
+ let result = data.map(item =>
   item).flat().filter(item => item.type == "furniture")
- console.log(tes);
- const jsonString = JSON.stringify(tes)
- fs.writeFile('./data/furniture.json', jsonString, err => {
-  if (err) {
-   console.log('Error writing file', err)
-  } else {
-   console.log('Successfully wrote file')
-  }
- })
+ console.log(result);
+ createFileJson("furnitures", result)
 }
 
 // soal3(inventory)
 
 //4. Find all items was purchased at 16 Januari 2020, and save it to purchased-at-2020-01-16.json.
 const soal4 = (data) => {
- let tes = data.map(item =>
+ let result = data.map(item =>
   item).flat().filter(item => new Date(item.purchased_at * 1000).toLocaleDateString() === "1/16/2020")
- console.log(tes);
-
- const jsonString = JSON.stringify(tes)
- fs.writeFile('./data/purchased-at-2020-01-16.json', jsonString, err => {
-  if (err) {
-   console.log('Error writing file', err)
-  } else {
-   console.log('Successfully wrote file')
-  }
- })
+ console.log(result);
+ createFileJson("purchased-at-2020-01-16", result)
 }
 
 // soal4(inventory)
 
 // 5. Find all items with brown color, all-browns.json.
 const soal5 = (data) => {
- let tes = data.map(item =>
+ let result = data.map(item =>
   item).flat().filter(item => item.tags[2] === "brown")
- console.log(tes);
-
- const jsonString = JSON.stringify(tes)
- fs.writeFile('./data/all-browns.json', jsonString, err => {
-  if (err) {
-   console.log('Error writing file', err)
-  } else {
-   console.log('Successfully wrote file')
-  }
- })
+ console.log(result);
+ createFileJson("all-browns", result)
 }
 
-soal5(inventory)
+// soal5(inventory)
